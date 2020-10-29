ac83489a76d8339127d46c69495a8652c48c3f6b
rangel-pci <rangelpereiracarneiro@gmail.com>
2020-10-28 22:51:46 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,300 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+import { 
+  Highlight, Highlight2, Header, Wrap, ToggleMenu, Nav, Logo, Main, Container,
+  About, Skill, Blocks, FloatingBlock, Project, FlexGrid, Contact, Footer,
+  ProjectModal
+} from './styles';
+
+const projects = {
+  minhaloji: {
+    title: 'Minhaloji.com',
+    text: () =>{
+      return(
+        <div>
+          <p>
+          Acesse em: <a href="https://minhaloji.com" target="_blank">minhaloji.com</a>
+          <br/>
+          Sobre: A minhaloji.com é uma plataforma onde vendedores podem criar sua própria página de vendas, adicionar produtos e serviços e ainda receber pedidos via WhatsApp (o cliente monta o pedido na página e envia pelo WhatsApp).
+          </p>
+          <hr/>
+          <p>
+          Neste projeto foi desenvolvido no Back End uma api com <Highlight>Laravel</Highlight> usando <Highlight>JSON Web Token</Highlight> como método de autenticação e <Highlight>AWS S3</Highlight> para o armazenamento em nuvem das imagens de produtos, logos, banners etc, e um blog em <Highlight>WordPress</Highlight>.
+          <br/>
+          No Front End foi usado <Highlight2>JQuery</Highlight2> para manipulação do DOM + <Highlight2>axios</Highlight2> para as requisições AJAX ao servidor.
+          </p>
+        </div>
+      );
+    },
+  },
+
+  pwm: {
+    title: 'PWM',
+    text: () =>{
+      return(
+        <div>
+          <p>
+          Disponível em: <a href="https://github.com/rangel-pci/pwmui" target="_blank">github.com/rangel-pci</a>
+          <br/>
+          Sobre: A PWM é um sitema focado em jogadores, e funciona como uma rede social permitindo a interação entre usuários, o usuário pode adicionar jogos a sua lista, exibir tag/nickname de outras plataformas como PSN, Twitch, Xbox etc, e ainda avaliar outros jogadores como legal ou tóxico.
+          </p>
+          <hr/>
+          <p>
+          Foi construida uma api com <Highlight>PHP OO</Highlight> puro seguindo o padrão MVC + autenticação com <Highlight>JWT</Highlight>.
+          <br/>
+          No Front End foi usado <Highlight2>React JS</Highlight2> + algumas ferramentas como <Highlight2>axios</Highlight2> para as requests HTTP e <Highlight2>React Router</Highlight2> para o controle de rotas.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        </div>
+      );
+    },
+  },
 
-export default App;
+  covid19panel: {
+    title: 'Painel COVID-19',
+    text: () =>{
+      return(
+        <div>
+          <p>
+          Acesse em: <a href="https://covid-19panel.herokuapp.com/" target="_blank">covid-19panel.herokuapp.com</a>
+          <br/>
+          Sobre: Painel com informações atualizadas em tempo real a respeito do vírus COVID-19 no Brasil e seus estados.
+          </p>
+          <hr/>
+          <p>
+          Aplicação feita em <Highlight2>React JS</Highlight2> consumindo e se mantendo atualizada diariamente com dados recebidos através da api do Ministério da Saúde.
+          </p>
+        </div>
+      );
+    },
+  },
+
+  gustavoRiether: {
+    title: 'Gustavo Riether',
+    text: () =>{
+      return(
+        <div>
+          <p>
+          Sobre: Landing Page simples feita em <Highlight>WordPress</Highlight> utilizando o plugin <Highlight2>Elementor Page Builder</Highlight2>.
+          </p>
+        </div>
+      );
+    },
+  },
+
+  ecoleta: {
+    title: 'Ecoleta',
+    text: () =>{
+      return(
+        <div>
+          <p>
+          Disponível em: <a href="https://github.com/rangel-pci/ecoletanlw" target="_blank">covid-19panel.herokuapp.com</a>
+          <br/>
+          Sobre: Com o Ecoleta é possível registrar e econtrar locais que coletem materiais recicláveis.
+          </p>
+          <hr/>
+          <p>
+          Back End feito em <Highlight>Node.JS|Express.JS</Highlight> utilizando o template engine <Highlight>Nunjucks</Highlight> e a api do IBGE para consulta de estados e cidades.
+          <br/>
+          No front End foi utilizado além de <Highlight2>HTML</Highlight2> e <Highlight2>CSS</Highlight2>, <Highlight2>JavaScript</Highlight2> puro para pequenas animações e requests com Fetch API.
+          </p>
+        </div>
+      );
+    },
+  },
+};
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      toggleMenuOn: 0,
+      projects: projects,
+      projectModal: {display: 'none', action: 'hide', title:'', text: () => false},
+    }
+  }
+
+  toggleMenu = () => {
+    const { toggleMenuOn } = this.state
+
+    toggleMenuOn ? this.setState({ toggleMenuOn: 0 }) : this.setState({ toggleMenuOn: 1 });    
+  }
+
+  showModalProject = (project) => {
+    this.setState({ projectModal: { display: 'block', action: 'show', title: project.title, text: project.text }});
+    document.body.style.overflow = "hidden";
+  }
+
+  hideModalProject = () => {
+    this.setState({ projectModal: { display: 'none', action: 'hide', title: '', text: () => false }});
+    document.body.style.overflowY = "scroll";
+  }
+
+  render(){
+    const { projects, projectModal } = this.state;
+
+    return (
+      <div>
+        <Header>
+          <Wrap>
+            <Logo><img src={process.env.PUBLIC_URL + '/assets/logo.svg'} height="60px" alt=""/></Logo>
+
+            <ToggleMenu on={this.state.toggleMenuOn} onClick={this.toggleMenu}><div></div><div></div><div></div></ToggleMenu>
+
+            <Nav on={this.state.toggleMenuOn}>
+              <ul>
+                <li><a href="#about">Sobre</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#projects">Projetos</a></li>
+                <li><a href="#contact">Contato</a></li>
+              </ul>
+            </Nav>
+          </Wrap>
+        </Header>
+
+        {/* About me */}
+        <Main>
+          <About id="about" imageUrl={process.env.PUBLIC_URL + '/assets/profile_img.jpg'} imageUrl2={process.env.PUBLIC_URL + '/assets/profile_img_desktop.jpg'}>
+            <Container className="grid">
+              <img className="perfil-img" src={process.env.PUBLIC_URL + '/assets/profile_img_desktop.png'} alt="foto de Rangel Pereira" />
+              
+              <div>
+                <h1>Rangel Pereira</h1>
+                <p>
+                  Brasiliense, 20 anos, técnico em Desenvolvimento de Sistemas. Desenvolvedor <Highlight><strong>PHP</strong></Highlight> e <Highlight><strong>JavaScript</strong></Highlight>.
+                </p>
+              </div>
+            </Container>
+          </About>
+
+          {/* My skills */}
+          <Skill id="skills">
+            <Container className="flex-c">
+              <div>
+                <h2>Minhas Skills</h2>
+                <p>Trabalho com desenvolvimento de sites e sistemas web usando as seguintes tecnologias.</p>
+
+                <Blocks>
+                  <FloatingBlock time="2">PHP</FloatingBlock>
+                  <FloatingBlock time="1.5">JavaScript</FloatingBlock>
+                  <FloatingBlock time="2">Laravel</FloatingBlock>
+                  <FloatingBlock time="1.5">React JS</FloatingBlock>
+                  <FloatingBlock time="2">JQuery</FloatingBlock>
+                  <FloatingBlock time="1.5">Bootstrap</FloatingBlock>
+                  <FloatingBlock time="2">HTML</FloatingBlock>
+                  <FloatingBlock time="1.5">CSS</FloatingBlock>
+                  <FloatingBlock time="2">MySQL</FloatingBlock>
+                  <FloatingBlock time="1.5">Git</FloatingBlock>
+                </Blocks>
+                <h2>Ferramentas</h2>
+                <Blocks>
+                  <FloatingBlock time="2">Sublime Text</FloatingBlock>
+                  <FloatingBlock time="1.5">Figma</FloatingBlock>
+                  <FloatingBlock time="2">Photoshop</FloatingBlock>
+                  <FloatingBlock time="1.5">Insomnia</FloatingBlock>
+                  <FloatingBlock time="2">Trello</FloatingBlock>
+                  <FloatingBlock time="1.5">...</FloatingBlock>
+                </Blocks>
+              </div>
+
+              <div>
+                <p>Também implemento projetos utilizando métodos e padrões como MVC, RESTful e JWT.</p>
+                <a href="#projects"><img width="30px" src={process.env.PUBLIC_URL + '/assets/arrow-down.svg'} /></a>
+              </div>
+            </Container>
+          </Skill>
+
+          {/* My projects */}
+          <Project id="projects">
+            <Container>
+             <h2>Projetos</h2>
+             <p>Estes são alguns dos meus projetos, clique sobre a imagem para informações detalhadas.</p>
+
+             <FlexGrid>
+                <button onClick={() => this.showModalProject(projects.minhaloji)}>
+                  <img src={process.env.PUBLIC_URL + '/assets/png1.png'} alt="projeto 1" />
+                  <span><Highlight>Laravel</Highlight> + <Highlight>AWS S3</Highlight> + <Highlight2>JQuery</Highlight2></span>
+                </button>
+                <button onClick={() => this.showModalProject(projects.pwm)}>
+                  <img src={process.env.PUBLIC_URL + '/assets/png2.png'} alt="projeto 2" />
+                  <span><Highlight>PHP</Highlight> + <Highlight2>React JS</Highlight2></span>
+                </button>
+                <button onClick={() => this.showModalProject(projects.covid19panel)}>
+                  <img src={process.env.PUBLIC_URL + '/assets/png3.png'} alt="projeto 3" />
+                  <span><Highlight2>React JS</Highlight2></span>
+                </button>
+                <button onClick={() => this.showModalProject(projects.gustavoRiether)}>
+                  <img src={process.env.PUBLIC_URL + '/assets/png4.png'} alt="projeto 4" />
+                  <span><Highlight>WordPress</Highlight></span>
+                </button>
+                <button onClick={() => this.showModalProject(projects.ecoleta)}>
+                  <img src={process.env.PUBLIC_URL + '/assets/png5.png'} alt="projeto 5" />
+                  <span><Highlight>Node.js</Highlight> + <Highlight2>JavaScript</Highlight2></span>
+                </button>
+             </FlexGrid>
+
+             <p>Mais projetos disponíveis no meu <a href="https://github.com/rangel-pci" target="_blank"><Highlight>GitHub</Highlight></a></p>
+            </Container>
+          </Project>
+
+          {/* My contact */}
+          <Contact id="contact" imageUrl={process.env.PUBLIC_URL + '/assets/contact.jpg'}>
+            <Container className="flex-c">
+              <h2>Contato</h2>
+              <p>Entre em contato para mais informações.</p>
+
+                <Blocks>
+                  <a href="https://www.linkedin.com/in/rangel-pereira-a5a4031b3/" target="_blank">
+                  <FloatingBlock time="2">
+                      <img src={process.env.PUBLIC_URL + '/assets/linkedin.svg'} width="25px" alt="linkedin" />
+                      <span>Linkedin</span>
+                  </FloatingBlock>
+                  </a>
+                  <a href="https://github.com/rangel-pci/" target="_blank">
+                  <FloatingBlock time="1.5">
+                      <img src={process.env.PUBLIC_URL + '/assets/github.svg'} width="25px" alt="github" />
+                      <span>GitHub</span>
+                  </FloatingBlock>
+                  </a>
+                  <a href="mailto:rangel.pci@gmail.com" target="_blank">
+                  <FloatingBlock time="2">
+                      <img src={process.env.PUBLIC_URL + '/assets/email.svg'} width="25px" alt="e-mail" />
+                      <span>rangel.pci@gmail.com</span>
+                  </FloatingBlock>
+                  </a>
+                  <a href="https://api.whatsapp.com/send?phone=5561992156634&text=Olá" target="_blank">
+                  <FloatingBlock time="1.5">
+                      <img src={process.env.PUBLIC_URL + '/assets/whatsapp.svg'} width="25px" alt="whatsapp" />
+                      <span>WhatsApp</span>
+                  </FloatingBlock>
+                  </a>
+                </Blocks>
+            </Container>
+          </Contact>
+          <Footer>
+            <Container>
+              <p><a href="#about"><Highlight>rangelpereira.com</Highlight></a> desenvolvido com <Highlight2>React JS</Highlight2> + <Highlight2>Styled Components</Highlight2></p>
+            </Container>
+          </Footer>
+        </Main>
+        
+        <ProjectModal display={projectModal.display} action={projectModal.action}>
+          <Container>
+            <div className="header">
+              <h2>{projectModal.title}</h2>
+            </div>
+            <hr/>
+
+            <div className="content">
+              {projectModal.text()}
+            </div>
+            <button className="return" onClick={this.hideModalProject}>
+              <img src={process.env.PUBLIC_URL + '/assets/left-arrow.svg'} width="25px" alt="" /><span><strong>Voltar</strong></span>
+            </button>
+          </Container>
+        </ProjectModal>
+
+      </div>
+    );
+  }
+}
---
+++ src/styles.js
@@ -0,0 +1,552 @@
+import styled, { css, keyframes } from 'styled-components';
+
+export const Header = styled.header`
+	background: #000;
+	padding: 0 24px;
+
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+
+	z-index: 100;
+
+	@media(min-width: 800px) {
+    	button{
+    		display: none;
+    	}
+
+    	height: 75.5px;
+
+    	nav{
+    		position: initial;
+    	
+			width: 100%;		
+
+			padding: 0;
+
+			border-radius: 0 0 0 5px;
+
+			ul{
+				height: 75.5px;
+				display: flex;
+				justify-content: flex-end;
+				align-items: center;
+			}
+			li{
+				text-align: center;
+				margin: 0 14px;
+				border-bottom: 0;
+			}
+			a{
+				padding: 8px;
+				color: #00E963;
+				display: block;
+			}
+
+			a:hover{
+				border-bottom: 2px solid #00E963;
+			}
+    	}
+  	}
+`;
+
+
+export const Wrap = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	width: 100%;
+	margin: 0 auto;
+	
+	max-width: 1200px;
+`;
+
+export const Container = styled.div`
+	width: 100%;
+	margin: 0 auto;
+	
+	max-width: 1200px;
+`;
+
+export const ToggleMenu = styled.button`
+	display: flex;
+	flex-direction: column;
+
+	background: #000;
+	border: none;
+
+	padding: 14px 0;
+	margin: 14px 0;
+
+	div{
+		width: 30px;
+		background: #00E963;
+		height: 2.5px;
+
+		margin: 2px 10px;
+
+		border-radius: 50px;
+
+		transition: 0.5s all;
+	}
+
+	${
+		props => props.on && css 
+		`
+			div:nth-child(3){
+				background: #000;
+			}
+			div:nth-child(1){
+				transform: rotate(45deg);
+			}
+			div:nth-child(2){
+				transform: rotate(-45deg);
+			}
+		`
+	}
+`;
+
+export const Nav = styled.nav`
+	background: #000;
+
+	position: fixed;
+	top: 75px;
+	right: -70%;
+
+	width: 70%;
+
+	display: flex;
+	justify-content: center;
+
+	padding: 24px 0;
+
+	border-radius: 0 0 0 5px;
+
+	z-index: 99;
+
+	transition: 0.5s all;
+
+	nav{
+		width: 100%;
+	}
+	ul{
+		list-style: none;
+		width: 100%;
+	}
+	li{
+		text-align: center;
+		margin: 14px;
+		border-bottom: 1px solid #4f4f4f;
+	}
+	a{
+		padding: 8px;
+		color: #00E963;
+		display: block;
+	}
+
+	${
+		props => props.on && css 
+		`
+			right: 0;
+		`
+	}
+`;
+
+export const Logo = styled.h6`
+	color: #00E963;
+`;
+
+export const Main = styled.main`
+	width: 100%;a
+`;
+
+// About me
+export const About = styled.section`
+	padding-top: 75.5px;
+
+	color: #fff;
+
+	height: calc(100vh);
+	overflow: hidden;
+	
+	position: relative;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-end;
+
+	padding: 0 24px;
+
+	border-radius: 0 0 50px 50px;
+
+	h1{
+		font-size: 32px;
+		background: #00E5A9;
+		border-radius: 50px;
+		padding: 8px 0;
+		text-align: center;
+	}
+	p{
+		font-size: 21px;
+		margin-top: 24px;
+		margin-bottom: 80px;
+		text-align: justify;
+	}
+
+	:before{
+		content: " ";
+		position: absolute;
+		top: 75.5px;
+		left: 0;
+
+		width: 100%;
+		height: 100%;
+
+		background-color: #000;
+		background-image: url(${props => props.imageUrl});
+		background-repeat: no-repeat;
+		background-position: top;
+		background-size: cover;
+
+		z-index: -2;
+	}
+
+	:after{
+		content: " ";
+		position: absolute;
+		top: 75.5px;
+		left: 0;
+
+		width: 100%;
+		height: 100%;
+
+		background: linear-gradient(to bottom, #00E963, #EA00D9);
+		opacity: 0.6;
+
+		z-index: -1;
+	}
+
+	.perfil-img{
+		display: none;
+	}
+
+	@media(min-width: 800px) {
+		.perfil-img{
+			display: block;
+			width: 200px;
+			height: 200px;
+
+			object-fit: cover;
+
+			border-radius: 100%;
+		}
+
+		.grid{
+			display: grid;
+			grid-template-columns: 250px auto;
+			grid-template-rows: auto;
+		}
+
+		:before{
+			content: none;
+		}
+
+		:after{
+			background: #000;
+			opacity: 1;
+		}
+
+		span{
+			font-size: 22px;
+		}
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+
+		border-radius: 0;
+
+		h1{
+			display: inline-block;
+			padding: 8px 48px;
+			background: #00E963;
+		}
+		p{
+			max-width: 600px;
+			margin-bottom: 0;
+		}
+	}	
+`;
+
+export const Skill = styled.section`
+	background: #fff;
+
+	min-height: 100vh;
+	overflow: hidden;
+
+	padding: 0 24px;
+
+	text-align: center;
+	color: #4f4f4f;
+
+	padding-top: 75.5px;
+
+	h2{
+		margin-top: 24px;
+	}
+	p{
+		font-size: 18px;
+		margin: 14px 0;
+	}
+
+	.flex-c{
+		height: 100%;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+	}
+`;
+
+export const Blocks = styled.div`
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+
+	margin: 14px 0;
+
+	div{
+		font-size: 21px;
+		color: #4f4f4f;
+		background: #fff;
+
+		border: 1px solid #f1f1f1;
+		border-radius: 5px;
+		box-shadow: 0px 2px 25px -16px rgba(0, 0, 0, 0.5);
+		
+		padding: 8px 24px;
+		margin: 2px 2px;
+	}
+`;
+
+const Fade = keyframes`
+	0% { transform: translateY(0);}
+	50% { transform: translateY(5px);}
+	100% { transform: translateY(0);}
+`;
+
+export const FloatingBlock = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	img{
+		margin-right: 8px;
+	}
+    span{
+    	border: none;
+    	box-shadow: none;
+    }
+
+    animation: ${Fade} ${props => props.time}s linear infinite;
+`;
+
+export const Project = styled.section`
+	background: #000;
+
+	min-height: 100vh;
+
+	overflow: hidden;
+
+	padding: 0 24px;
+
+	text-align: center;
+	color: #f1f1f1;
+
+	padding-top: 75.5px;
+
+	h2{
+		margin-top: 24px;
+	}
+	p{
+		font-size: 18px;
+		margin: 14px 0;
+	}
+`;
+
+export const FlexGrid = styled.div`
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+	grid-template-rows: repeat(auto-fill, 1fr);
+	grid-gap: 14px;
+
+	button{
+		width: 100%;
+		cursor: pointer;
+
+		background: none;
+		border: none;
+	}
+	button img{
+		width: 100%;
+	}
+`;
+
+export const Contact = styled.section`
+	background: rgba(255,255,255, 0.95);
+
+	overflow: hidden;
+
+	padding: 80px 24px;
+
+	text-align: center;
+	color: #4f4f4f;
+
+	min-height: 400px;
+
+	p{
+		font-size: 18px;
+		margin: 14px 0;
+	}
+
+	position: relative;
+
+	:before{
+		content: "";
+
+		position: absolute;
+		top: 0;
+		left: 0;
+
+		width: 100%;
+		height: 100%;
+
+		background-image: url(${props => props.imageUrl});
+		background-repeat: no-repeat;
+		background-position: bottom;
+		background-size: cover;
+
+		z-index: -1;
+	}
+
+	@media(min-width: 800px) {
+		min-height: 100vh;
+		p{
+			margin-bottom: 48px;
+		}
+	}
+`;
+
+export const Footer = styled.footer`
+	background: #000;
+
+	overflow: hidden;
+
+	text-align: center;
+	color: #f1f1f1;
+
+	border-top: 2px solid #f1f1f1;
+
+	p{
+		font-size: 13px;
+		margin: 14px 0;
+	}
+`;
+
+const showModal = keyframes`
+	0% { transform: translateX(-100%);}
+	50% { transform: translateX(-10%);}
+	100% { transform: translateX(0);}
+`;
+
+const hideModal = keyframes`
+	0% { transform: translateX(0);}
+	50% { transform: translateX(-10%);}
+	100% { transform: translateX(-100%);}
+`;
+
+export const ProjectModal = styled.span`
+	display: ${props => props.display};
+	position: fixed;
+	top: 0;
+	left: 0;
+
+	width: 100%;
+	height 100%;
+
+	overflow-y: scroll;
+
+	padding: 0 24px;
+	padding-bottom: 60px;
+
+	background: #f1f1f1;
+
+	z-index: 100;
+
+	color: #4f4f4f;
+
+	.header{
+		padding: 14px 0;
+	}
+
+	h2{
+		font-size: 24px;
+		text-align: center;
+	}
+
+	hr{
+		width: 200px;
+		margin: 8px auto;
+		background: #fff;
+	}
+
+	.content p{
+		padding-top: 24px;
+		font-size: 18px;
+	}
+
+	.return{
+		position: fixed;
+		bottom: 0;
+		left: 0;
+
+		width: 100%;
+
+		display: flex;
+		justify-content: center;
+		font-size: 18px;
+		font-weigth: 600;
+
+		padding: 14px 0;
+		border: none;
+		background: #f1f1f1;
+
+		box-shadow: 0 0 5px rgba(0,0,0,0.25);
+	}
+
+	.return img{
+		margin-right: 8px;
+	}
+
+	${
+		props => props.action === 'show' && css 
+		`
+    		animation: ${showModal} 0.5s linear;
+		`
+	}
+	${
+		props => props.action === 'hide' && css 
+		`
+    		animation: ${hideModal} 0.5s linear;
+		`
+	}
+`;
+
+export const Highlight = styled.span`
+	color: #00E963;
+`;
+
+export const Highlight2 = styled.span`
+	color: #21B6E5;
+`;
